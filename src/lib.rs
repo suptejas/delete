52a4872479344eea9e829a0bf553176c4be26490
XtremeDevX <xtremedevx@gmail.com>
2021-07-01 18:16:17 +0400

    feat: add docstrings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+/// Fast, easy deletion of files and folders with async and cross-platform support.
 use futures::stream::{FuturesUnordered, StreamExt};
 
 fn walkdir(path: &str) -> (Vec<String>, Vec<String>) {
@@ -14,49 +15,158 @@ fn walkdir(path: &str) -> (Vec<String>, Vec<String>) {
     (files_vec, folders_vec)
 }
 
-async fn delete_files(files: &[String]) {
+async fn priv_delete_files(files: &[String]) {
     for f in files {
         tokio::fs::remove_file(f).await.unwrap();
     }
 }
 
-async fn delete_folder(folders: &[String]) {
+async fn priv_delete_folder(folders: &[String]) {
     for f in folders {
         let _ = tokio::fs::remove_dir_all(f).await;
     }
 }
 
-pub fn remove_file(path: &str) -> std::io::Result<()> {
+/// Delete a file from the filesystem.
+///
+/// Uses `std::fs` internally.
+/// ## Examples
+/// ```
+/// use delete::{delete_file};
+///
+/// fn main() {
+///   // Delete file.txt
+///   delete_file("file.txt").unwrap();
+/// }
+/// ```
+///
+pub fn delete_file(path: &str) -> std::io::Result<()> {
     std::fs::remove_file(path)?;
     Ok(())
 }
 
-pub async fn remove_file_async(path: &str) -> std::io::Result<()> {
+/// Delete a file from the filesystem using `async` and `tokio`.
+///
+/// Uses `tokio::fs` internally.
+/// ## Examples
+/// ```
+/// use delete::{delete_file_async};
+///
+/// #[tokio::main]
+/// async fn main() {
+///   // Delete file.txt
+///   delete_file_async("file.txt").await.unwrap();
+/// }
+/// ```
+///
+pub async fn delete_file_async(path: &str) -> std::io::Result<()> {
     tokio::fs::remove_file(path).await?;
     Ok(())
 }
 
-pub fn remove_dir(path: &str) -> std::io::Result<()> {
+/// Delete an empty folder from the filesystem.
+///
+/// Uses `std::fs` internally.
+/// ## Examples
+/// ```
+/// use delete::{delete_folder};
+///
+/// fn main() {
+///   // Delete tests folder
+///   delete_folder("tests").unwrap();
+/// }
+/// ```
+///
+pub fn delete_folder(path: &str) -> std::io::Result<()> {
     std::fs::remove_dir(path)?;
     Ok(())
 }
 
-pub async fn remove_dir_async(path: &str) -> std::io::Result<()> {
+/// Delete an empty folder from the filesystem using `async` and `tokio`.
+///
+/// Uses `tokio::fs` internally.
+/// ## Examples
+/// ```
+/// use delete::{delete_folder_async};
+///
+/// #[tokio::main]
+/// async fn main() {
+///   // Delete tests folder
+///   delete_folder_async("tests").await.unwrap();
+/// }
+/// ```
+///
+pub async fn delete_folder_async(path: &str) -> std::io::Result<()> {
     tokio::fs::remove_dir(path).await?;
     Ok(())
 }
 
-pub fn remove_dir_all(path: &str) -> std::io::Result<()> {
+/// Delete a folder from the filesystem after recursively deleting all its contents.
+///
+/// Uses `std::fs` internally.
+/// ## Examples
+/// ```
+/// use delete::{delete_folder_all};
+///
+/// fn main() {
+///   // Delete node_modules folder and all its contents.
+///   delete_folder_all("node_modules").unwrap();
+/// }
+/// ```
+///
+pub fn delete_folder_all(path: &str) -> std::io::Result<()> {
     std::fs::remove_dir_all(path)?;
     Ok(())
 }
 
-pub async fn remove_dir_all_async(path: &str) -> std::io::Result<()> {
+/// Delete a folder from the filesystem after recursively deleting all its contents using `async` and `tokio`.
+///
+/// Uses `tokio::fs` internally.
+/// ## Examples
+/// ```
+/// use delete::{delete_folder_all_async};
+///
+/// #[tokio::main]
+/// async fn main() {
+///   // Delete node_modules folder and all its contents.
+///   delete_folder_all_async("node_modules").await.unwrap();
+/// }
+/// ```
+///
+pub async fn delete_folder_all_async(path: &str) -> std::io::Result<()> {
     tokio::fs::remove_dir_all(path).await?;
     Ok(())
 }
 
-pub async fn rapid_remove_dir_all(
+/// Rapidly delete a folder from the filesystem after recursively deleting all its contents using `async` and `tokio`.
+///
+/// Benchmarked to be 2-3x faster than `std::fs::remove_dir_all()`
+///
+/// Uses tokio workers to delete files and folders parallely.
+///
+/// ## Parameters
+/// path: `&str` - path to the folder to delete
+///
+/// (Optional) folders_chunk_size: `Option<u64>` - number of folders to be deleted per worker.
+///
+/// if this value is lower, more workers are spawned.
+/// (Optional) files_chunk_size: `Option<u64>` - number of files to be deleted per worker.
+///
+/// if this value is lower, more workers are spawned.
+///
+/// Uses `tokio::fs` internally.
+/// ## Examples
+/// ```
+/// use delete::{rapid_delete_dir_all};
+///
+/// #[tokio::main]
+/// async fn main() {
+///   // Delete node_modules folder and all its contents 2x faster.
+///   rapid_delete_dir_all("node_modules", None, None).await.unwrap();
+/// }
+/// ```
+///
+pub async fn rapid_delete_dir_all(
     path: &str,
     folders_chunk_size: Option<u64>,
     files_chunk_size: Option<u64>,
@@ -77,7 +187,7 @@ pub async fn rapid_remove_dir_all(
 
     for chunk in chunks {
         workers.push(async move {
-            delete_files(chunk).await;
+            priv_delete_files(chunk).await;
         });
     }
 
@@ -97,7 +207,7 @@ pub async fn rapid_remove_dir_all(
 
     for folder in folders {
         workers.push(async move {
-            delete_folder(folder).await;
+            priv_delete_folder(folder).await;
         })
     }
 
